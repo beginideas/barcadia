4f2f2c36dc4652907a2acffe941f047ad97d750e
beginideas <usman.uddin@beginideas.com>
2022-08-07 14:12:02 -0400

    Update PerksModule.js

--- src/components/PerksModule/PerksModule.js
+++ src/components/PerksModule/PerksModule.js
@@ -15,11 +15,7 @@ const PerksModule = () => {
         layout="constrained"
         placeholder="tracedSVG"
       />
-   <div className="banner__content2" style="
-    top: 25px;
-">
-
-            
+   <div className="banner__content2" style="top: 25px;">            
               <h1>Our Clients</h1>
    </div>
 

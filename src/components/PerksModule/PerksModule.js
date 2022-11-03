4c6c2bb37fbf841cdc498099179b9b43096a425a
beginideas <usman.uddin@beginideas.com>
2022-11-03 19:15:02 -0400

    Update PerksModule.js

--- src/components/PerksModule/PerksModule.js
+++ src/components/PerksModule/PerksModule.js
@@ -82,17 +82,7 @@ const PerksModule = () => {
           />
         </Perk>
     
-      <Perk
-          title=" "
-         
-        >
-          <StaticImage
-            src="../../../static/logos/Sandia_National_Laboratories_logo.svg.png"
-            alt="Sandia National Laboratories Logo"
-            layout="constrained"
-            placeholder="tracedSVG"
-          />
-        </Perk>
+    
       </div>
     </PerksModuleStyles>
   )
